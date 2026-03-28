19abbe55ac62547ff9349465f1616cb8c01571dd
Sufiyan <mdsufiyanabdulhaque@gmail.com>
2026-03-28 19:48:06 +0530

    still debuggin

--- src/config/database.js
+++ src/config/database.js
@@ -6,8 +6,6 @@ const { ComplaintEntity } = require("../entities/Complaint");
 const { NotificationEntity } = require("../entities/Notification");
 const OnboardingReminderEntity = require("../entities/OnboardingReminder");
 
-// console.log("NODE_ENV:", process.env.NODE_ENV);
-// console.log("DB_CONN:", process.env.DB_CONN);
 const isProduction = process.env.NODE_ENV === "production";
 console.log("isProduction statement :", isProduction)
 
